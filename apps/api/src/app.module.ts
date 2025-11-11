9e8033521bb6c055c8a01f1c7fd5c0436b1efeba
yepezmarcos <136519685+yepezmarcos@users.noreply.github.com>
2025-11-11 13:14:02 -0500

    chore: move app.module.ts to src directory

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { HealthModule } from "./health/health.module";
+import { StudentsModule } from "./students/students.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    HealthModule,
+    StudentsModule
+  ]
+})
+export class AppModule {}
